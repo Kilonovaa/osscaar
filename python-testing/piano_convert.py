7d3ba9830922560b5109f1c130fafe462a3ede1a
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-10-07 14:39:19 +0300

    make sound louder

--- python-testing/piano_convert.py
+++ python-testing/piano_convert.py
@@ -1,5 +1,11 @@
 #function to convert pixel to midi to wav file
 from midi2audio import FluidSynth
+from pydub import AudioSegment
 
 synth = FluidSynth('congas MW_1_0.SF2')
-synth.midi_to_audio('deb_menu.mid',"output.wav", gain=2)
+
+synth.midi_to_audio('deb_menu.mid',"output.wav")
+
+song = AudioSegment.from_wav("output.wav")
+song_20_db_louder = song + 20
+song_20_db_louder.export("louder.wav", "wav")
