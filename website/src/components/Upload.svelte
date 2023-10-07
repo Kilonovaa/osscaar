ccd839f2849c181446ad2293571f494a49e15f91
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-10-07 15:32:49 +0300

    websocket with file upload

---
+++ website/src/components/Upload.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { get } from "svelte/store"
+  import { socketObject } from "../stores/socket"
+  const socket = get(socketObject)
+
+  socket.on("upload_response", (data) => {
+    console.log(data)
+  })
+  function upload() {
+    const file = document.getElementById("file").files[0]
+    const reader = new FileReader()
+    // reader.readAsDataURL(file)
+    reader.readAsArrayBuffer(file)
+
+    reader.onload = (fileEvent) => {
+      const arrayBuffer = fileEvent.target.result
+      const uint8Array = new Uint8Array(arrayBuffer)
+      socket.emit("upload", uint8Array, (response) => {})
+    }
+  }
+</script>
+
+<input type="file" id="file" name="file" />
+<input type="button" value="Upload" on:click={upload} />
