4625d21b0f387ac02c8de6f74a7f3e4b381b3bed
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-10-07 18:14:06 +0300

    .

--- website/src/lib/socket.js
+++ website/src/lib/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client"
 // export const socketObject = writable(io("ws://localhost:8000", {}))
 export const socketObject = writable(
   io("wss://wsnasa2023.lazar.lol", {
+    path: "/"
     transports: ["websocket", "polling", "flashsocket"],
   })
 )
