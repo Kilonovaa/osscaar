583929daae6c83c77ab54fa574dad28c4f4a6e9a
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-10-07 17:40:50 +0300

    Update socket.js

--- website/src/lib/socket.js
+++ website/src/lib/socket.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store"
 import { io } from "socket.io-client"
 // export const socketObject = writable(io("ws://localhost:8000", {}))
-export const socketObject = writable(io("ws://wsnasa2023.lazar.lol", {}))
+export const socketObject = writable(io("wss://wsnasa2023.lazar.lol", {}))
