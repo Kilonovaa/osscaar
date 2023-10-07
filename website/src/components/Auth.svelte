ad286823ab64ce21136901b4e053b94d8afdb3e5
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-10-07 17:07:46 +0300

    added supabase auth

---
+++ website/src/components/Auth.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { supabase } from "../lib/supabase"
+  let email, password, passwordConfirm
+  function register() {
+    if (password !== passwordConfirm) {
+      alert("Passwords do not match")
+      return
+    }
+    supabase.auth.signUp({
+      email,
+      password,
+    })
+  }
+  function login() {
+    supabase.auth.signIn({
+      email,
+      password,
+    })
+  }
+</script>
+
+<h1>Login</h1>
+<input type="email" bind:value={email} />
+<input type="password" bind:value={password} />
+
+<h1>Register</h1>
+<input type="email" bind:value={email} />
+<input type="password" bind:value={password} />
+<input type="password" bind:value={passwordConfirm} />
--- website/src/components/Upload.svelte
+++ website/src/components/Upload.svelte
@@ -1,16 +1,18 @@
 <script>
   import { get } from "svelte/store"
-  import { socketObject } from "../stores/socket"
+  import { socketObject } from "../lib/socket"
   const socket = get(socketObject)
+  let progress = 0
 
   socket.on("upload_response", (data) => {
     console.log(data)
+    progress = 0 // reset progress after upload
   })
+
   function upload() {
     const file = document.getElementById("file").files[0]
     console.log(file)
     const reader = new FileReader()
-    // reader.readAsDataURL(file)
     reader.readAsArrayBuffer(file)
 
     reader.onload = (fileEvent) => {
@@ -18,8 +20,15 @@
       const uint8Array = new Uint8Array(arrayBuffer)
       socket.emit("upload", uint8Array, (response) => {})
     }
+
+    reader.onprogress = (event) => {
+      if (event.lengthComputable) {
+        progress = Math.round((event.loaded / event.total) * 100)
+      }
+    }
   }
 </script>
 
 <input type="file" id="file" name="file" />
 <input type="button" value="Upload" on:click={upload} />
+<progress value={progress} max="100" />
