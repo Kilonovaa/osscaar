b7e22fa74a972accf97ecc4aa13dfd5a4e2ea9c6
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-10-08 09:54:25 +0300

    working file upload to supabase

--- websockets/python/app.py
+++ websockets/python/app.py
@@ -3,8 +3,10 @@ import socketio
 import random
 import string
 import os
-
+from requests_toolbelt.multipart.encoder import MultipartEncoder, MultipartEncoderMonitor
+import requests
 from supabase import create_client, Client
+import sys
 
 url = "https://nwhobhigrgxtpnwydpxj.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53aG9iaGlncmd4dHBud3lkcHhqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjY4Njg1MiwiZXhwIjoyMDEyMjYyODUyfQ.7UxyPZo5PLIEuuntEAXi01t0ZrEc7ZcReQRA08af1qU"
@@ -12,7 +14,7 @@ supabase: Client = create_client(url, key)
 
 
 sio = socketio.AsyncServer(
-    async_mode='aiohttp', cors_allowed_origins='*', max_http_buffer_size=1024 ** 3)
+    async_mode='aiohttp', cors_allowed_origins='*', max_http_buffer_size=1024 ** 3, logger=False)
 app = web.Application()
 
 
@@ -29,51 +31,13 @@ async def background_task():
 
 
 @sio.event
-async def my_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']}, room=sid)
-
-
-@sio.event
-async def my_broadcast_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']})
-
-
-@sio.event
-async def join(sid, message):
-    await sio.enter_room(sid, message['room'])
-    await sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
-                   room=sid)
-
-
-@sio.event
-async def leave(sid, message):
-    await sio.leave_room(sid, message['room'])
-    await sio.emit('my_response', {'data': 'Left room: ' + message['room']},
-                   room=sid)
-
-
-@sio.event
-async def close_room(sid, message):
-    await sio.emit('my_response',
-                   {'data': 'Room ' + message['room'] + ' is closing.'},
-                   room=message['room'])
-    await sio.close_room(message['room'])
-
-
-@sio.event
-async def my_room_event(sid, message):
-    await sio.emit('my_response', {'data': message['data']},
-                   room=message['room'])
-
-
-@sio.event
 async def disconnect_request(sid):
     await sio.disconnect(sid)
 
 
 @sio.event
 async def connect(sid, environ):
-    await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
+    print('Client connected')
 
 
 @sio.event
@@ -86,25 +50,113 @@ def createPath(path):
         os.makedirs(path)
 
 
+def create_callback(sio, sid, encoder):
+    encoder_len = len(encoder)
+
+    def callback(monitor):
+        progress = monitor.bytes_read / encoder_len
+        print(f"Upload progress: {progress * 100}%")
+        sio.emit('upload_response', {
+            "id": sid,
+            "progress": progress * 100
+        }, room=sid)
+
+    return callbac
+
+
 @sio.on('upload')
 async def upload(sid, file_data):
-    try:
-        random_base64 = ''.join(random.choices(
-            string.ascii_lowercase + string.digits, k=10))
-        folder = f"./tmp/{sid}/"
-        filepath = f'{folder}{random_base64}.mp4'
-        createPath(folder)
-        # with open(filepath, 'wb') as file:
-        #     file.write(file_data)
-        print(f"File uploaded successfully: {filepath}")
-        response = {'message': 'success'}
+    # try:
+    #     random_base64 = ''.join(random.choices(
+    #         string.ascii_lowercase + string.digits, k=10))
+    #     folder = f"./tmp/{sid}/"
+    #     filepath = f'{folder}{random_base64}.mp4'
+    #     createPath(folder)
+    #     # with open(filepath, 'wb') as file:
+    #     #     file.write(file_data)
+    #     print(f"File uploaded successfully: {filepath}")
+    #     response = {'message': 'success'}
 
-        supabase.storage.from_("data").upload(
-            file=file_data, path=f"./public/{sid}{random_base64}.mp4", file_options={"content-type": "video/mp4"})
-    except Exception as e:
-        print(f"Failed to upload file: {str(e)}")
-        response = {'message': 'failure', 'error': str(e)}
-    await sio.emit('upload_response', response, room=sid)
+    #     supabase.storage.from_("data").upload(
+    #         file=file_data, path=f"./public/{sid}{random_base64}.mp4", file_options={"content-type": "video/mp4"})
+    # except Exception as e:
+    #     print(f"Failed to upload file: {str(e)}")
+    #     response = {'message': 'failure', 'error': str(e)}
+    # await sio.emit('upload_response', response, room=sid)
+
+    # try:
+    #     random_base64 = ''.join(random.choices(
+    #         string.ascii_lowercase + string.digits, k=10))
+    #     folder = f"./tmp/{sid}/"
+    #     filepath = f'{folder}{random_base64}.mp4'
+    #     createPath(folder)
+    #     print(f"File uploaded successfully: {filepath}")
+
+    #     # Create form data
+    #     multipart_data = MultipartEncoder(
+    #         fields={
+    #             'cacheControl': '3600',
+    #             '': (filepath, file_data, 'video/mp4')
+    #         }
+    #     )
+
+    #     monitor = MultipartEncoderMonitor(
+    #         multipart_data, create_callback(sio, sid, multipart_data))
+
+    #     headers = {
+    #         "Content-Type": monitor.content_type,
+    #         **supabase.headers,
+    #     }
+
+    #     response = requests.post(
+    #         f"{url}/storage/v1/object/data/{sid}{random_base64}.mp4",
+    #         data=monitor,
+    #         headers=headers
+    #     )
+
+    #     if response.status_code == 200:
+    #         print("The transfer is complete.")
+    #         response = {'message': 'success'}
+    #     else:
+    #         print(f"Failed to upload file: {response.content}")
+    #         response = {'message': 'failure', 'error': str(response.content)}
+
+    # except Exception as e:
+    #     print(f"Failed to upload file: {str(e)}")
+    #     response = {'message': 'failure', 'error': str(e)}
+
+    # await sio.emit('upload_response', response, room=sid)
+    supaURL = "https://nwhobhigrgxtpnwydpxj.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53aG9iaGlncmd4dHBud3lkcHhqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjY4Njg1MiwiZXhwIjoyMDEyMjYyODUyfQ.7UxyPZo5PLIEuuntEAXi01t0ZrEc7ZcReQRA08af1qU"
+    bucket = "data"
+    file_path = "test.mp4"
+
+    file_size = os.path.getsize(file_path)
+    file_size = sys.getsizeof(file_data)
+
+    url = f"{supaURL}/storage/v1/object/{bucket}/{file_path}"
+
+    multipart_encoder = MultipartEncoder(
+        fields={
+            'file': (os.path.basename(file_path), file_data, 'video/mp4')
+        }
+    )
+
+    monitor = MultipartEncoderMonitor(multipart_encoder, lambda monitor: print(
+        f"Upload progress: {monitor.bytes_read} / {file_size} bytes uploaded"))
+
+    headers = {
+        'Authorization': f'Bearer {key}',
+        'Content-Type': monitor.content_type
+    }
+
+    response = requests.post(url, data=monitor, headers=headers)
+
+    if response.status_code == 200:
+        print("Upload successful.")
+    else:
+        print(f"Upload failed with status code: {response.status_code}")
+        print(response.text)
 
 # app.router.add_static('/static', 'static')
 
---
+++ websockets/python/testing/upload.py
@@ -0,0 +1,61 @@
+import os
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder, MultipartEncoderMonitor
+
+supaURL = "https://nwhobhigrgxtpnwydpxj.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53aG9iaGlncmd4dHBud3lkcHhqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjY4Njg1MiwiZXhwIjoyMDEyMjYyODUyfQ.7UxyPZo5PLIEuuntEAXi01t0ZrEc7ZcReQRA08af1qU"
+bucket = "data"
+file_path = "test.mp4"
+
+file_size = os.path.getsize(file_path)
+
+url = f"{supaURL}/storage/v1/object/{bucket}/{file_path}"
+
+multipart_encoder = MultipartEncoder(
+    fields={
+        'file': (os.path.basename(file_path), open(file_path, 'rb'), 'video/mp4')
+    }
+)
+
+monitor = MultipartEncoderMonitor(multipart_encoder, lambda monitor: print(
+    f"Upload progress: {monitor.bytes_read} / {file_size} bytes uploaded"))
+
+headers = {
+    'Authorization': f'Bearer {key}',
+    'Content-Type': monitor.content_type
+}
+
+response = requests.post(url, data=monitor, headers=headers)
+
+if response.status_code == 200:
+    print("Upload successful.")
+else:
+    print(f"Upload failed with status code: {response.status_code}")
+    print(response.text)
+
+
+def upload(file, progressFunction, finishFunction):
+    file_size = os.path.getsize(file)
+
+    url = f"{supaURL}/storage/v1/object/{bucket}/{file}"
+
+    multipart_encoder = MultipartEncoder(
+        fields={
+            'file': (os.path.basename(file), open(file, 'rb'), 'video/mp4')
+        }
+    )
+
+    monitor = MultipartEncoderMonitor(
+        multipart_encoder, lambda monitor: progressFunction(monitor.bytes_read, file_size))
+
+    headers = {
+        'Authorization': f'Bearer {key}',
+        'Content-Type': monitor.content_type
+    }
+
+    response = requests.post(url, data=monitor, headers=headers)
+
+    if response.status_code == 200:
+        finishFunction(True)
+    else:
+        finishFunction(False)
