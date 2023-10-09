9293652b6eaf715d86a30a8692e39c4b72dde297
Smartyguy001 <109079347+Smartyguy001@users.noreply.github.com>
2023-10-09 04:31:47 +0300

    processed Cosmic Wonders sample

--- python-testing/example_generator.py
+++ python-testing/example_generator.py
@@ -5,6 +5,7 @@ import os
 def shorthandGetMidisFromVideo(fileName: str):
     getMidisFromVideo("../video-stash/" + fileName + ".mp4", "../video-stash/" + fileName, emptyCallback)
 
-shorthandGetMidisFromVideo("Animation of Comet 2I_Borisov")
-shorthandGetMidisFromVideo("Cosmic Reef_ NGC 2014 & NGC 2020")
-shorthandGetMidisFromVideo("Flight Through the Orion Nebula in Infrared Light")
+# shorthandGetMidisFromVideo("Animation of Comet 2I_Borisov")
+# shorthandGetMidisFromVideo("Cosmic Reef_ NGC 2014 & NGC 2020")
+# shorthandGetMidisFromVideo("Flight Through the Orion Nebula in Infrared Light")
+shorthandGetMidisFromVideo("Cosmic Wonders")
